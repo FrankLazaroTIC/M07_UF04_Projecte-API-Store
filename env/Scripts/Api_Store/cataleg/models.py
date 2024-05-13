75ba5c2f727d5db2b8f630b4019d026e3b03056b
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2024-05-13 19:44:17 +0200

    Model product fet

--- env/Scripts/Api_Store/cataleg/models.py
+++ env/Scripts/Api_Store/cataleg/models.py
@@ -1,3 +1,11 @@
+# catalog/models.py
+
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    nom = models.CharField(max_length=100)
+    descripcio = models.TextField()
+    preu = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
