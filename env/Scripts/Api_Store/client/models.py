4533236a00fad89c4ff8313af57d9e0d2905fe4d
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2024-05-13 19:37:12 +0200

    Model Client Fet

--- env/Scripts/Api_Store/client/models.py
+++ env/Scripts/Api_Store/client/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Client(models.Model):
@@ -7,6 +8,8 @@ class Client(models.Model):
     cognoms = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=128)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.nom + " " + self.cognoms
