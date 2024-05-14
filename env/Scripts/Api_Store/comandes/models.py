d534050c0d56f7e390cb5f3ed929673aba4aefbd
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2024-05-14 18:19:57 +0200

    Model Cart fet

--- env/Scripts/Api_Store/comandes/models.py
+++ env/Scripts/Api_Store/comandes/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+# orders/models.py
 
-# Create your models here.
+from django.db import models
+from cart.models import Cart
+
+class Order(models.Model):
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
