773fb3b3f0fde3d930225f0cb958715bc3241c42
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2024-05-14 18:21:34 +0200

    Cart Model Fet

--- env/Scripts/Api_Store/carreto/models.py
+++ env/Scripts/Api_Store/carreto/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# cart/models.py
+
+from django.db import models
+from catalog.models import Product
+from client.models import Client
+
+class Cart(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
